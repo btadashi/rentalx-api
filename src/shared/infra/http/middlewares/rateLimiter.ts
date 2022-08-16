f8a0fb31c5ebb8c5683f9a202221dc3ccc56f900
tadashi <tadashi.b@gmail.com>
2022-08-15 22:06:22 -0300

    Sentry installed

--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -3,33 +3,6 @@ import { Request, Response, NextFunction } from 'express';
 import { RateLimiterRedis } from 'rate-limiter-flexible';
 import { AppError } from '@shared/errors/AppErrors';
 
-// const redisClient = redis.createClient({
-//   host: process.env.REDIS_HOST,
-//   port: Number(process.env.REDIS_PORT),
-// });
-
-// const limiter = new RateLimiterRedis({
-//   storeClient: redisClient,
-//   keyPrefix: 'rateLimiter',
-//   points: 10,
-//   duration: 5,
-// });
-
-// export default async function rateLimiter(
-//   request: Request,
-//   response: Response,
-//   next: NextFunction
-// ): Promise<void> {
-//   try {
-//     await limiter.consume(request.ip);
-
-//     return next();
-//   } catch (err) {
-//     throw new AppError("Too many requests", 429);
-//   }
-// }
-
-
 export default async function rateLimiter(
   request: Request,
   response: Response,
@@ -43,8 +16,7 @@ export default async function rateLimiter(
     },
   });
 
-  await redisClient.connect(); // essa aqui
-
+  await redisClient.connect();
 
   const limiter = new RateLimiterRedis({
     storeClient: redisClient,
