1fc2e2a604c193d68b91d6f4566b0cf41f687fd7
tadashi <tadashi.b@gmail.com>
2022-08-15 22:05:59 -0300

    Sentry installed

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,6 +4,8 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 import swaggerUi from 'swagger-ui-express';
 import { router } from './routes';
@@ -17,12 +19,23 @@ import { AppError } from '@shared/errors/AppErrors';
 import upload from '@config/upload';
 import rateLimiter from './middlewares/rateLimiter';
 
-
 createConnection();
 
 const app = express();
 
 app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 
@@ -34,6 +47,8 @@ app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 app.use(cors());
 
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if (err instanceof AppError) {
--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -3,28 +3,60 @@ import { Request, Response, NextFunction } from 'express';
 import { RateLimiterRedis } from 'rate-limiter-flexible';
 import { AppError } from '@shared/errors/AppErrors';
 
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-});
+// const redisClient = redis.createClient({
+//   host: process.env.REDIS_HOST,
+//   port: Number(process.env.REDIS_PORT),
+// });
 
-const limiter = new RateLimiterRedis({
-  storeClient: redisClient,
-  keyPrefix: 'rateLimiter',
-  points: 10,
-  duration: 5,
-});
+// const limiter = new RateLimiterRedis({
+//   storeClient: redisClient,
+//   keyPrefix: 'rateLimiter',
+//   points: 10,
+//   duration: 5,
+// });
+
+// export default async function rateLimiter(
+//   request: Request,
+//   response: Response,
+//   next: NextFunction
+// ): Promise<void> {
+//   try {
+//     await limiter.consume(request.ip);
+
+//     return next();
+//   } catch (err) {
+//     throw new AppError("Too many requests", 429);
+//   }
+// }
+
 
 export default async function rateLimiter(
   request: Request,
   response: Response,
   next: NextFunction
 ): Promise<void> {
+  const redisClient = redis.createClient({
+    legacyMode: true,
+    socket: {
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT),
+    },
+  });
+
+  await redisClient.connect(); // essa aqui
+
+
+  const limiter = new RateLimiterRedis({
+    storeClient: redisClient,
+    keyPrefix: "rateLimiter",
+    points: 5,
+    duration: 5,
+  });
   try {
     await limiter.consume(request.ip);
 
     return next();
   } catch (err) {
-    throw new AppError("Too many requests", 429);
+    throw new AppError(err.message, 429);
   }
 }
